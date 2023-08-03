40d3ac13c49c21ab2e8ffa702db75d934f49629c
jbrak <joe@brakoniecki.com>
2023-08-03 14:26:49 +0100

    Added Box.py

--- MakingShapes/box.py
+++ MakingShapes/box.py
@@ -1,3 +1,23 @@
 from vpython import *
 
+# First Box
 box()
+
+# Adjusting Parameters
+box(pos=vector(1, 0, 0), size=vector(1, .3, .2), color=color.yellow)
+
+# Creating a Better Purple
+purpleHSV = (vector(148, 0, 211)) / 255
+purpleRGB = color.hsv_to_rgb(purpleHSV)
+
+box(pos=vector(-1, 0, 0), size=vector(1, .2, .2), color=purpleHSV)
+
+# Creating a Mowhawk
+box(pos = vector(0, .6, 0), size = vector(.5, .2, 1), color = color.red)
+
+# Creating The Eyeballs
+box(pos = vector(-.2,.2,.6), size = vector(.2, .2, .2), color = color.black)
+box(pos = vector(.2,.2,.6), size = vector(.2, .2, .2), color = color.black)
+
+# Creating the Mouth
+box(pos = vector(0,-.2,.6), size= vector(.75, .2, .2), color = color.blue)
